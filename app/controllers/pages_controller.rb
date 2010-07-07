d8faa76566734d3373737120fac7b04d1bcd064d
Brandon Cordell <leadandrock@gmail.com>
2010-07-07 06:40:37 -0400

    Created functional tests for pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def about
+    @title = 'About'
   end
 
 end
--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  # quick testing of the title_for_layout helper
+  test "page title without setting title variable" do
+    get :index
+    assert_select 'title', 'Cannibook'
+  end
+  
+  test "page title with title variable set" do
+    get :about
+    assert_select 'title', 'About &ndash; Cannibook'
+  end
+  
+  test "get index page" do
+    get :index
+    assert_response :success
+  end
+  
+  test "get about page" do
+    get :about
+    assert_response :success
   end
 end
