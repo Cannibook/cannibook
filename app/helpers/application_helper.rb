9879470bd5460d3162b1f611f43617f9267e86db
Brandon Cordell <leadandrock@gmail.com>
2010-07-08 13:34:09 -0400

    Cleaned up more html, added a few helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,26 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # output a title for the page
   def title_for_layout
     if @title.nil?
       "Cannibook"
     else
       "#{@title} &ndash; Cannibook"
     end
+  end
+  
+  # outputs true or false whether the page is the current_page
+  def current?(controller_name, action_name = nil)
+    if current_page?(:controller => controller_name, :action => action_name)
+      'current'
+    else
+      nil
+    end
+  end
+  
+  # get the name of the controller and output it for the body tags id paramater
+  def id_for_body
+    id = request[:controller].to_s
+    " id=\"#{id}\""
   end
 end
