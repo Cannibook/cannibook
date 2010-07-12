6a01610c02c62e698fd9f5cbe615651810f5cbd5
Brandon Cordell <leadandrock@gmail.com>
2010-07-12 03:34:37 -0400

    Added tagging with acts_as_tagging_on

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def index
+    smell_tags = Strain.tag_counts_on(:smells)
+    taste_tags = Strain.tag_counts_on(:tastes)
+    @tags = smell_tags + taste_tags
   end
 
   def about
--- app/models/strain.rb
+++ app/models/strain.rb
@@ -2,4 +2,6 @@ class Strain < ActiveRecord::Base
   belongs_to :dispensary
   
   validates_presence_of :name
+  
+  acts_as_taggable_on :smells, :tastes
 end
